9458f9c37f203253866da4809815aea0a0f9e0cd
Barry Mullan <mullan@gmail.com>
2014-11-20 09:23:35 -0500

    changed

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -4,6 +4,6 @@ package sample;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");          // the old SysO-statement
+        System.out.pri!ntln("Hello World");          // the old SysO-statement
     }
 }
